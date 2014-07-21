35375aa09e441280bb14c93bce82dc2540bdc6e5
derek <derek.snow@gmail.com>
2014-07-21 11:40:04 -0500

    removed unused code and styling

--- add-sub.js
+++ add-sub.js
@@ -6,16 +6,15 @@ $(document).ready(function() {
         var num1 = +$('#num1').val();
         var num2 = +$('#num2').val();
         var result = "";
-        var op = "plus";
+        var operator = "plus";
         if (validInt(num1) && validInt(num2)) {
             if ($(this).attr('id') == 'add') {
                 result = num1 + num2;
             } else if ($(this).attr('id') == 'sub') {
                 result = num1 - num2;
-                op = "minus";
+                operator = "minus";
             }
-            $('.op').empty().append(op);
-            $('.result').empty().append(num1 + " " + op + " " + num2 + " is " + result).show();
+            $('.result').empty().append(num1 + " " + operator + " " + num2 + " is " + result).show();
 
         } else {
             $('.result').empty().append("Error - You must enter an integer for X and Y.").addClass('error').show();
